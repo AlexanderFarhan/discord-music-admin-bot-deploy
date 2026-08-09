0c6df7d488a8f83848586772b21f91afd1e8378c
AlexanderFarhan <152864083+AlexanderFarhan@users.noreply.github.com>
2026-08-09 07:28:06 +0700

    Add src/commands/index.js

---
+++ src/commands/index.js
@@ -0,0 +1,7 @@
+import { helpCommand } from './help.js';
+import { infoCommand } from './info.js';
+import { moderationCommand } from './moderation.js';
+import { musicCommand } from './music.js';
+
+export const commands = [musicCommand, moderationCommand, infoCommand, helpCommand];
+
